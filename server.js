73e5a7a271ee8292374efeb83a2841df01e21d41
Onur Alp Akin <onralpakin@gmail.com>
2023-09-10 07:40:55 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+const api = require('./src/api');
+
+const express = require('express')
+const app = express()
+
+const DOSYA_ISMI = `${__dirname}/output/zimmet.docx`
+
+// todo bunu kaldir
+app.get('/', (req, res) => {
+    res.send('<h2>Zimmet tutanagini almak icin "/zimmet/x" yazin. x yerine asset tag veya kullanici adi </h2><br/><h2>Orn: /zimmet/00173 ya da /zimmet/o.akin</h2>')
+})
+
+app.get('/:usernameOrTag', (req, res) => {
+    const re = /^\d{5}$/g;
+
+    // tag 
+    if (re.test(req.params.usernameOrTag)) {
+        api.getByTag(req.params.usernameOrTag).then(async hardware => {
+            const hardwareResult = api.checkValidAsset(hardware);
+            // ok
+            if (typeof hardwareResult === 'boolean' && hardwareResult) {
+                api.giveJsonToPython(hardware);
+                // res.send(hardware);
+                res.download(DOSYA_ISMI);
+            } else if (typeof hardwareResult === 'string') { //patladi hatayi goster
+                res.send(hardwareResult);
+            }
+        });
+    } else { // user
+        let valid = true;
+        api.getIdFromUsername(req.params.usernameOrTag).then(async user_id => {
+            if (user_id) {
+                api.getAssetsFromId(user_id).then(assets => {
+                    assets.rows.forEach(hardware => {
+                        const hardwareResult = api.checkValidAsset(hardware);
+                        if (typeof hardwareResult === 'string') { //patladi hatayi goster
+                            valid = false;
+                            res.send(hardwareResult);
+                        }
+                    });
+                    if (valid) {
+                        api.giveJsonToPython(assets);
+                        // res.send(assets)
+                        res.download(DOSYA_ISMI);
+                    }
+                });
+            } else {
+                res.send("kullanici adi hatali")
+            }
+        });
+    }
+});
+
+app.listen(3000)
