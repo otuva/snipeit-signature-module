aed142aef389b1c88dc334c22bc4c05867aad7dd
Onur Alp AKIN <onralpakin@gmail.com>
2023-09-29 12:51:31 +0300

    holy hell

--- src/api.js
+++ src/api.js
@@ -6,35 +6,35 @@ const execSync = require("child_process").execSync;
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 // variables
-const API_TOKEN = "<API_TOKEN>"
-const HOST = "snipeit.example.com"
+const SNIPEIT_TOKEN = process.env.SNIPEIT_TOKEN
+const SNIPEIT_HOST = process.env.SNIPEIT_HOST
 
-// zimmette hata var mi kontrol etmek icin. hata yoksa true varsa string hata mesaji dondurur
-const checkValidAsset = (asset_json) => {
-    if (asset_json.last_checkout === null) {
-        return `Zimmet formundan once demirbas checkout edilmeli 'TAG: ${asset_json.asset_tag}'`;
-    } else if (asset_json.manufacturer === null) {
-        return `Demirbasin ureticisi bos olmamali 'TAG: ${asset_json.asset_tag}'`;
-    } else if (asset_json.serial === "") {
-        return `Demirbasin seri numarasi girilmemis. Demirbasin seri numarasi yoksa seri numarasini '-' olarak guncelleyin. 'TAG: ${asset_json.asset_tag}'`;
-    } else if (asset_json.status === "error" && asset_json.messages === "Asset does not exist.") {
+// check if asset is valid for form. if it isn't return string error message, else return true
+const checkValidAsset = (assetJson) => {
+    if (assetJson.last_checkout === null) {
+        return `Zimmet formundan once demirbas checkout edilmeli 'TAG: ${assetJson.asset_tag}'`;
+    } else if (assetJson.manufacturer === null) {
+        return `Demirbasin ureticisi bos olmamali 'TAG: ${assetJson.asset_tag}'`;
+    } else if (assetJson.serial === "") {
+        return `Demirbasin seri numarasi girilmemis. Demirbasin seri numarasi yoksa seri numarasini '-' olarak guncelleyin. 'TAG: ${assetJson.asset_tag}'`;
+    } else if (assetJson.status === "error" && assetJson.messages === "Asset does not exist.") {
         return "Boyle bir tagi olan demirbas yok kontrol edin";
-    } else if (asset_json.status === "error") {
-        return `Bisiler patladi hata oldu: ${asset_json.messages}`
+    } else if (assetJson.status === "error") {
+        return `Bisiler patladi hata oldu: ${assetJson.messages}`
     } else {
         return true;
     }
 }
 
-// tag ile demirbas getir
-const getByTag = (asset_tag) => {
+// get asset by tag
+const getAssetByTag = (assetTag) => {
     return new Promise((resolve, reject) => {
         const options = {
             method: 'GET',
-            url: `https://${HOST}/api/v1/hardware/bytag/${asset_tag}?deleted=false`,
+            url: `https://${SNIPEIT_HOST}/api/v1/hardware/bytag/${assetTag}?deleted=false`,
             headers: {
                 accept: 'application/json',
-                Authorization: `Bearer ${API_TOKEN}`
+                Authorization: `Bearer ${SNIPEIT_TOKEN}`
             }
         };
 
@@ -50,15 +50,60 @@ const getByTag = (asset_tag) => {
     });
 }
 
-// kullanici adiyla id dondur
-const getIdFromUsername = (username) => {
+// get asset by id
+const getAssetById = (assetId) => {
     return new Promise((resolve, reject) => {
         const options = {
             method: 'GET',
-            url: `https://${HOST}/api/v1/users?limit=1&offset=0&sort=created_at&order=desc&username=${username}&deleted=false&all=false`,
+            url: `https://${SNIPEIT_HOST}/api/v1/hardware/${assetId}`,
             headers: {
                 accept: 'application/json',
-                Authorization: `Bearer ${API_TOKEN}`
+                Authorization: `Bearer ${SNIPEIT_TOKEN}`
+            }
+        };
+
+        axios
+            .request(options)
+            .then((response) => {
+                resolve(response.data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+const getAccessoryById = (accessoryId) => {
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: 'GET',
+            url: `https://${SNIPEIT_HOST}/api/v1/accessories/${accessoryId}`,
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${SNIPEIT_TOKEN}`
+            }
+        };
+
+        axios
+            .request(options)
+            .then((response) => {
+                resolve(response.data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+// return id by username
+const getIdByUsername = (username) => {
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: 'GET',
+            url: `https://${SNIPEIT_HOST}/api/v1/users?limit=1&offset=0&sort=created_at&order=desc&username=${username}&deleted=false&all=false`,
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${SNIPEIT_TOKEN}`
             }
         };
 
@@ -78,15 +123,15 @@ const getIdFromUsername = (username) => {
     });
 }
 
-// id ile kullanici ustundeki demirbaslari getir
-const getAssetsFromId = (user_id) => {
+// get all assets from id
+const getAssetsByUserId = (userId) => {
     return new Promise((resolve, reject) => {
         const options = {
             method: 'GET',
-            url: `https://${HOST}/api/v1/users/${user_id}/assets`,
+            url: `https://${SNIPEIT_HOST}/api/v1/users/${userId}/assets`,
             headers: {
                 accept: 'application/json',
-                Authorization: `Bearer ${API_TOKEN}`
+                Authorization: `Bearer ${SNIPEIT_TOKEN}`
             }
         };
 
@@ -101,7 +146,61 @@ const getAssetsFromId = (user_id) => {
     });
 }
 
-// jsonu docx olusturmak icin pitona ver
+// query user id to get checkin activities
+const getCheckinsByUserId = (userId) => {
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: 'GET',
+            url: `https://${SNIPEIT_HOST}/api/v1/reports/activity?limit=10&offset=0&target_type=user&target_id=${userId}&action_type=checkin%20from&order=desc&sort=created_at`,
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${SNIPEIT_TOKEN}`
+            }
+        };
+
+        axios
+            .request(options)
+            .then((response) => {
+                resolve(response.data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+const getDetailedCheckinItemsByUsername = (userId) => {
+    const rows = []
+
+    return new Promise((resolve, reject) => {
+        getCheckinsByUserId(userId)
+            .then(async (checkedInItems) => {
+
+                for await (const singleCheckin of checkedInItems.rows) {
+                    if (singleCheckin.item.type === "asset") {
+                        await getAssetById(singleCheckin.item.id).then((checkedinAsset) => {
+                            rows.push(checkedinAsset)
+                        })
+                    } else if (singleCheckin.item.type === "accessory") {
+                        await getAccessoryById(singleCheckin.item.id).then((checkedinAccessory) => {
+                            rows.push(checkedinAccessory)
+                        })
+                    }
+                }
+
+                resolve({
+                    totalCheckins: rows.length,
+                    rows
+                })
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+
+}
+
+// give json to python for creating docx
 const giveJsonToPython = (json) => {
     // console.log(`${__dirname}/py/venv/bin/python ${__dirname}/py/handle_json.py '${JSON.stringify(json)}'`)
     // const pythonProcess = execSync(`${__dirname}/py/venv/bin/python`, [`${__dirname}/py/handle_json.py`, `'${JSON.stringify(json)}'`]);
@@ -109,7 +208,9 @@ const giveJsonToPython = (json) => {
 }
 
 module.exports.checkValidAsset = checkValidAsset;
-module.exports.getByTag = getByTag;
-module.exports.getIdFromUsername = getIdFromUsername;
-module.exports.getAssetsFromId = getAssetsFromId;
+module.exports.getAssetByTag = getAssetByTag;
+module.exports.getIdByUsername = getIdByUsername;
+module.exports.getAssetsByUserId = getAssetsByUserId;
+module.exports.getCheckinsByUserId = getCheckinsByUserId;
+module.exports.getDetailedCheckinItemsByUsername = getDetailedCheckinItemsByUsername;
 module.exports.giveJsonToPython = giveJsonToPython;
